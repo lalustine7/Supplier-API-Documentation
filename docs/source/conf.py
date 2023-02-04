c10241c602f668287e277cf2a3362ebef58f0916
Lal Kumar Rai <lal@spicesresearch.com>
2023-02-04 08:51:10 +0545

    Updated Extenstions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-     "sphinxcontrib.httpdomain",
+    #  "sphinxcontrib.httpdomain",
      "sphinx-prompt"
 ]
 

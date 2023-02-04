e70833db4cb0e67479988300fa2199e2847c1a4d
Lal Kumar Rai <lal@spicesresearch.com>
2023-02-04 20:22:20 +0545

    Added Extension Back

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    "sphinx-prompt"
+    "sphinx-prompt",
+    "sphinx-tabs",
 ]
 
 intersphinx_mapping = {

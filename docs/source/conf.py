ea8aba5a14bc635a239fa27c56fbe6d491dbefd4
Lal Kumar Rai <lal@spicesresearch.com>
2023-02-04 10:14:16 +0545

    Changed Project Information

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Supplier API Documentation'
+copyright = '2023, Lal Kumar Rai'
+author = 'Lal Kumar Rai'
 
 release = '0.1'
 version = '0.1.0'
